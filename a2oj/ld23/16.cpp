9e6ef05865485c42b319182eaab325ca5150d5a1
Ankitesh Kumar <18ucs110@lnmiit.ac.in>
2021-07-31 05:20:33 +0530

    diff 1 problems from ld23

---
+++ a2oj/ld23/16.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long ;
+using pii = pair<int , int>;
+using pll = pair<ll, ll>;
+
+
+//pairs
+#define ss second
+#define ff first
+
+
+// vectors
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
+//arrays
+#define mem(x , y) memset(x , y , sizeof(x) )
+
+const int N = 1e5 + 10 , mod =  1000000007;
+
+//helper funcs
+ll cdiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+
+
+void solve() {
+
+    int n ; cin >> n;
+    string str; cin >> str;
+
+    int cnt = 0;
+
+    FOR(i,0,n){
+    	if(str[i] == 'x')cnt++;
+    }
+
+    int ans = abs(n/2 - cnt) ;
+
+    FOR(i,0,n){
+    	if(str[i] == 'x' && cnt > n/2 ){
+    		str[i] = 'X';
+    		cnt--;
+    	}else if(str[i] == 'X' && cnt < n/2 ){
+    		str[i] = 'x';
+    		cnt++;
+    	}
+    }
+
+    cout << ans << "\n";
+    cout << str ;
+
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+
+
+    #ifndef ONLINE_JUDGE
+        freopen("/home/ankitesh/Desktop/competitive/input.txt", "r", stdin);
+        freopen("/home/ankitesh/Desktop/competitive/output.txt", "w", stdout);
+    #endif
+
+    // time_t start , end;
+
+    // time(&start);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+
+    // time(&end);
+
+    // printf("time: %f", (double)end - start );
+
+    return 0;
+}
+
+
---
+++ a2oj/ld23/18.cpp
@@ -0,0 +1,139 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long ;
+using pii = pair<int , int>;
+using pll = pair<ll, ll>;
+
+
+//pairs
+#define ss second
+#define ff first
+
+
+// vectors
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
+//arrays
+#define mem(x , y) memset(x , y , sizeof(x) )
+
+const int N = 1e5 + 10 , mod =  1000000007;
+
+//helper funcs
+ll cdiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+
+bool cmp(string s1 , string s2){
+	int n = s1.size() , m = s2.size();
+
+	FOR(i,0,min(n, m)){
+		if(s1[i] < s2[i] ){
+			return true;
+		}else if(s1[i] > s2[i]){
+			return false;
+		}
+	}
+
+	if(m > n)return true;
+
+	return false;
+
+}
+
+bool ispal(string str){
+	int n = str.size();
+
+	FOR(i,0,n/2){
+		if(str[i] != str[n-i-1]){
+			return false;
+		}
+	}
+
+	return true;
+
+}
+
+void func(string str , int st , int ed , int cnt , string tmp ,  string& ans){
+
+	if(cnt == 1){
+		FOR(i,st,ed){
+			if(ispal(tmp+str[i]) && cmp(ans , tmp + str[i])){
+				ans = tmp + str[i];
+				// cout << st << " " << i << " " << ans << "\n";
+			}
+		}
+		return;
+	}
+
+	FOR(i,st,ed){
+		func(str , i+1 , ed , cnt-1 , tmp + str[i] , ans );
+	}
+}
+
+
+void solve() {
+
+    string str; cin >> str;
+
+    int n = str.size();
+
+    string ans = "a";
+
+    FOR(i,1,n+1){
+    	func(str , 0 , n , i , "" , ans );
+    }
+
+    cout << ans << "\n";
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+
+
+    #ifndef ONLINE_JUDGE
+        freopen("/home/ankitesh/Desktop/competitive/input.txt", "r", stdin);
+        freopen("/home/ankitesh/Desktop/competitive/output.txt", "w", stdout);
+    #endif
+
+    // time_t start , end;
+
+    // time(&start);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+
+    // time(&end);
+
+    // printf("time: %f", (double)end - start );
+
+    return 0;
+}
+
+
